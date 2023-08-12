d855111084add35c04d08d21cab2c4372ebfa4a5
tiebanluyu <108140288+tiebanluyu@users.noreply.github.com>
2023-08-12 18:38:07 +0800

    大洗盘，完成

--- py版文件转bat/main.py
+++ py版文件转bat/main.py
@@ -3,17 +3,24 @@ from tkinter import filedialog
 import base64
 def main(n):
     #传入的没有用
+    
+    
     file_path = filedialog.askopenfilename(title="要选择什么文件")
-    bat_path = filedialog.asksaveasfilename(title="bat要保存在哪里")
-    with open(file_path,"br") as file:
-        content=file.read()
-        result=base64.b64encode(bytes(content))
-        #print(result)
-        result=str(result)[2:-1]
-        file_path=file_path.split("\")[-1]
-        result="""@echo off
+    bat_path = filedialog.asksaveasfilename(title="bat要保存在哪里",filetypes=(("Batch files", "*.bat"), ("All files", "*.*")))
+    print(file_path,bat_path,type(file_path))
+
+    import base64
+
+    # 读取文件并将其转换为Base64编码
+    with open(file_path, 'rb') as file:
+        encoded = base64.b64encode(file.read())
+    file_path=file_path.split("/")[-1].replace(" ","")    
+    result="""
+    
+@echo off
 goto CmdCode
------BEGIN CERTIFICATE-----"""+result+f"""
+-----BEGIN CERTIFICATE-----
+"""+encoded.decode('utf-8')+f"""
 -----END CERTIFICATE-----
 :CmdCode
 del {file_path} /F >NUL 2>NUL
@@ -21,11 +28,9 @@ certutil -decode %0 {file_path} >nul
 start "" {file_path}
 exit
 """
-
-
-    with open(bat_path,"w") as file:
-        file.write(str(result))
-    
+    # 将Base64编码写入另一个文件
+    with open(bat_path, 'w') as file:
+        file.write(result)
 
 
 win=tkinter.Tk()
